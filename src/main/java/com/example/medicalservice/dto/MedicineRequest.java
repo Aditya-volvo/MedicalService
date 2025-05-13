f6afea849d0edabe5cbdc94935f873f21e0e9a1a
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-13 10:47:50 +0530

    Created RequestDto and ResponseDto this is how we will restrict the user what data he can enter and what data he can read

---
+++ src/main/java/com/example/medicalservice/dto/MedicineRequest.java
@@ -0,0 +1,48 @@
+package com.example.medicalservice.dto;
+
+import com.example.medicalservice.enums.Form;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MedicineRequest {
+    @NotBlank
+    private String medicineName;
+
+    @NotBlank
+    private String medicineCategory;
+
+    @NotBlank
+    private List<String> medicineIngredients;
+
+    @NotBlank
+    private int dosageInMg;
+
+    @NotBlank
+    private Form form;
+
+    @NotBlank
+    private String manufacturer;
+
+    @NotBlank
+    private int stockQuantity;
+
+    @NotBlank
+    private LocalDate expireDate;
+
+    @NotBlank
+    private Double price;
+
+    @NotBlank
+    private String pharmacyId;
+}
---
+++ src/main/java/com/example/medicalservice/dto/MedicineResponse.java
@@ -0,0 +1,29 @@
+package com.example.medicalservice.dto;
+
+import com.example.medicalservice.enums.Form;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MedicineResponse {
+    private String medicineId;
+    private String medicineName;
+    private String medicineCategory;
+    private List<String> medicineIngredients;
+    private int dosageInMg;
+    private Form form;
+    private String manufacturer;
+    private int stockQuantity;
+    private LocalDate expireDate;
+    private Double price;
+    private String pharmacyId;
+}
