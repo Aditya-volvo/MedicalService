d255c8b79dab1b163fd1f5f0f5cea59caaa0ac68
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-13 10:13:45 +0530

    Created Medicine Entity implementing what should be mapped in the table

---
+++ src/main/java/com/example/medicalservice/model/Medicine.java
@@ -0,0 +1,56 @@
+package com.example.medicalservice.model;
+
+import com.example.medicalservice.enums.Form;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "medicine")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Medicine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "m_id")
+    private String medicineId;
+
+    @Column(name = "m_name")
+    private String medicine_Name;
+
+    @Column(name = "m_category")
+    private String medicineCategory;
+
+    @ElementCollection
+    @CollectionTable(name = "medicine_ingredients", joinColumns = @JoinColumn(name = "m_id"))
+    @Column(name = "m_Ingredients")
+    private List<String> medicineIngredients;
+
+    @Column(name = "m_dosage")
+    private int dosageInMg;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "m_from")
+    private Form form;
+
+    @Column(name = "manufacture")
+    private String manufacturer;
+
+    @Column(name = "m_stock")
+    private int stockQuantity;
+
+    @Column(name = "m_expire_at")
+    private LocalDate expireDate;
+
+    @Column(name = "m_price")
+    private Double price;
+
+
+}
