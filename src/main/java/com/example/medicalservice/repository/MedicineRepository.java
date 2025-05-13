5fa0f626b47375bfe9a875bb8967ad59d1f00872
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-13 13:28:19 +0530

    Modified MedicineRepository implemented Memberfunction findMedicineById()

--- src/main/java/com/example/medicalservice/repository/MedicineRepository.java
+++ src/main/java/com/example/medicalservice/repository/MedicineRepository.java
@@ -3,5 +3,8 @@ package com.example.medicalservice.repository;
 import com.example.medicalservice.model.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MedicineRepository extends JpaRepository<Medicine,String> {
+   Optional<Medicine>  findMedicineById(String medicineId);
 }
