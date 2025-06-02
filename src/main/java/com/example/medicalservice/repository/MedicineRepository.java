6ea808d2f7fb9f33a8c94ef99e7b3dfc3984cf6d
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-06-02 14:46:26 +0530

    added findMedicineById, findMedicineByPharmacyId operations.

--- src/main/java/com/example/medicalservice/repository/MedicineRepository.java
+++ src/main/java/com/example/medicalservice/repository/MedicineRepository.java
@@ -3,9 +3,13 @@ package com.example.medicalservice.repository;
 import com.example.medicalservice.model.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+import java.util.List;
 import java.util.Optional;
 
-public interface MedicineRepository extends JpaRepository<Medicine,String> {
+public interface MedicineRepository extends JpaRepository<Medicine,Long> {
    Optional<Medicine>  findMedicineById(Long medicineId);
 
+
+    List<Medicine> findMedicineByPharmacyId(Long pharmacyId);
 }
